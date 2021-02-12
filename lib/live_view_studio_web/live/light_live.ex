ba18f954ed57d96f7dcf8541edbc2151527e25c2
vishalsodani <vishalsodani@rediffmail.com>
2021-02-12 10:03:36 +0530

    added min and max as controller so values are rangebound between 0 and 100

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -62,13 +62,12 @@ defmodule LiveViewStudioWeb.LightLive do
   end
 
   def handle_event("up", _, socket) do
-    socket = update(socket, :brightness, &(&1 + 10))
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
     {:noreply, socket}
   end
 
   def handle_event("down", _, socket) do
-    brightness = socket.assigns.brightness - 10
-    socket = assign(socket, :brightness, brightness)
+    socket = update(socket, :brightness, &max(&1 - 10, 0))
     {:noreply, socket}
   end
 
